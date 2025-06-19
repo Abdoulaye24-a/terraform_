b6fe6621e31778dc90de67f0827a63f40153f7eb
Abdoulaye24-a <77053236+Abdoulaye24-a@users.noreply.github.com>
2025-06-19 23:47:45 +0000

    Update variables.tf

--- multi-cloud-nginx/terraform/aws/variables.tf
+++ multi-cloud-nginx/terraform/aws/variables.tf
@@ -1,15 +1,12 @@
 variable "aws_access_key" {
   description = "AWS Access Key"
   type        = string
+  default     = "" # Laissez vide si vous passez par les variables d'environnement
 }
 
 variable "aws_secret_key" {
   description = "AWS Secret Key"
   type        = string
   sensitive   = true
-}
-
-variable "aws_key_name" {
-  description = "AWS Key Pair Name"
-  type        = string
+  default     = "" # Laissez vide si vous passez par les variables d'environnement
 }
