01081ef0eb023b530de920c0fe62abe04e2beca7
Abdoulaye24-a <77053236+Abdoulaye24-a@users.noreply.github.com>
2025-06-20 08:33:01 +0000

    Delete multi-cloud-nginx/terraform/gcp directory

--- multi-cloud-nginx/terraform/gcp/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "gcp_public_ip" {
-  value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
-}
--- multi-cloud-nginx/terraform/gcp/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "project_id" {}
-variable "credentials_file" {}
-variable "public_key_path" {}
