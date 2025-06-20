38c287b6df8fa9c7a662ec47c446d3019d20264f
Abdoulaye24-a <77053236+Abdoulaye24-a@users.noreply.github.com>
2025-06-20 21:42:47 +0000

    Update main.tf

--- multi-cloud-nginx/terraform/aws/main.tf
+++ multi-cloud-nginx/terraform/aws/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = "us-east-1"
+  # Pas besoin de définir access_key / secret_key ici
+  # Terraform les prendra dans les variables d’environnement GitHub Actions
 }
 
 resource "aws_instance" "vm" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
-  key_name      = var.aws_key_name != "" ? var.aws_key_name : null
+  key_name      = var.aws_key_name
 
   tags = {
     Name = "vm-aws-nginx"
