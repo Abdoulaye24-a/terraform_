d039c2578bb904174ac0127bec6f68492d11cd18
Abdoulaye24-a <77053236+Abdoulaye24-a@users.noreply.github.com>
2025-06-20 07:45:39 +0000

    Update variables.tf

--- multi-cloud-nginx/terraform/aws/variables.tf
+++ multi-cloud-nginx/terraform/aws/variables.tf
@@ -1,18 +1,19 @@
+# variables.tf - Bonne configuration
 variable "aws_access_key" {
   description = "AWS Access Key"
   type        = string
-  default     = "" # Utilisera les variables d'environnement si vide
+  default     = "" # Utilisera les variables d'environnement
 }
 
 variable "aws_secret_key" {
   description = "AWS Secret Key"
   type        = string
   sensitive   = true
-  default     = "" # Utilisera les variables d'environnement si vide
+  default     = "" # Utilisera les variables d'environnement
 }
 
 variable "aws_key_name" {
   description = "AWS Key Pair Name"
   type        = string
-  default     = "" # Remplace par ta clé SSH AWS ou passe la via secrets
+  default     = "" # Doit être vide pour utiliser le secret GitHub
 }
